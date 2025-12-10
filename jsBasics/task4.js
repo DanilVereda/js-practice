a0ec7bfca7d540c083f3c5cf2d493d993a34a1ed
Danylo Vereda <danil.vereda247@gmail.com>
2025-12-10 23:27:27 +0200

    Add task4.js

---
+++ jsBasics/task4.js
@@ -0,0 +1,19 @@
+// Task 4.1
+let radiusCircle = 5;
+let squareCircle = Math.PI * Math.pow(radiusCircle, 2);
+
+console.log(`The square of circle is ${squareCircle.toFixed(2)}`);
+
+// Task 4.2
+let length = 7;
+let width = 8;
+let squareRectangle = length * width;
+
+console.log(`The square of rectangle is ${squareRectangle.toFixed(2)}`);
+
+// Task 4.3
+let radiusCylinder = 4.3;
+let heightCylinder = 10.25;
+let squareCylinder = Math.PI * Math.pow(radiusCylinder, 2) * heightCylinder;
+
+console.log(`The square of cylinder is ${squareCylinder.toFixed(2)}`);
